cf340c98f0a3b981b3d3c4b85886493bb17afbdd
Felipe Marques <felipemph2@gmail.com>
2022-08-04 19:39:32 -0300

    Adição da Página Events

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MarvelApiService } from 'src/app/services/marvel-api/marvel-api.service';
+import { EventDetailsComponent } from '../event-details/event-details.component';
 
 @Component({
   selector: 'app-events',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private marvelApi: MarvelApiService,
+    private eventDialog: MatDialog
+  ) { }
+
+  events:any=[];
+  showSearchResult: boolean = false;
+
+  totalLength:any;
+  page:number = 1;
 
   ngOnInit(): void {
+    this.showSearchResult=false;
+    this.marvelApi.getEvents().subscribe((result)=>{
+      console.log(result);
+      this.events = result.data.results;
+      this.totalLength = result.length;
+    })
+  }
+
+  openDetails(eventId:string){
+    this.eventDialog.open(EventDetailsComponent,{
+      data: eventId
+    })
   }
 
 }
--- src/app/services/marvel-api/marvel-api.service.ts
+++ src/app/services/marvel-api/marvel-api.service.ts
@@ -10,6 +10,7 @@ export class MarvelApiService {
   constructor(private http: HttpClient) { }
 
   baseUrl="https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8"
+  baseUrlEvents="https://gateway.marvel.com:443/v1/public/events?limit=100&ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8"
 
   getCharacters():Observable<any>{
     return this.http.get(this.baseUrl);
@@ -17,6 +18,25 @@ export class MarvelApiService {
 
   getCharacterDetails(characterId:string):Observable<any>{
     const charDetailsUrl=`https://gateway.marvel.com:443/v1/public/characters/${characterId}?ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8`
-    return this.http.get(charDetailsUrl)
+    return this.http.get(charDetailsUrl);
+  }
+
+  getCharacterByName(charName:string):Observable<any>{
+    const charNameUrl=`https://gateway.marvel.com:443/v1/public/characters?name=${charName}&ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8`
+    return this.http.get(charNameUrl);
+  }
+
+  getEvents():Observable<any>{
+    return this.http.get(this.baseUrlEvents);
+  }
+
+  getEventDetails(eventId:string):Observable<any>{
+    const eventDetailsUrl=`https://gateway.marvel.com:443/v1/public/events/${eventId}?ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8`;
+    return this.http.get(eventDetailsUrl);
+  }
+
+  getEventCharacters(eventId:string):Observable<any>{
+    const eventDetails2Url=`https://gateway.marvel.com:443/v1/public/events/${eventId}/characters?limit=100&ts=1&apikey=29a0ce3cb104e468f9082e31fd5e544f&hash=6edbb1d327d97efeded233382d8527e8`
+    return this.http.get(eventDetails2Url);
   }
 }
